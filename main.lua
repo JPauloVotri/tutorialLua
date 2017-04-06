664cfa40058e4f2e33b0b1adaad83622724a1a09
João Paulo Marques Votri <jpaulo.votri@hotmail.com>
2017-04-06 16:19:58 -0300

    Alterações e adições feitas no episódio 2

--- main.lua
+++ main.lua
@@ -1,18 +1,51 @@
+love.graphics.setDefaultFilter('nearest', 'nearest')
+
+enemy = {}
+enemies_controller = {}
+enemies_controller.enemies = {}
+enemies_controller.image = love.graphics.newImage("enemy.png")
+
 function love.load()
   player          = {}
   player.x        = 0
-  player.y        = 550
+  player.y        = 110
   player.bullets  = {}
   player.cooldown = 40
-  player.speed    = 10
+  player.speed    = 0.5
+  player.image    = love.graphics.newImage("player.png")
   player.fire     = function()
     if player.cooldown <= 0 then
       player.cooldown = 40
       bullet   = {}
-      bullet.x = player.x + 35
-      bullet.y = player.y
+      bullet.x = player.x + 4
+      bullet.y = player.y + 2
       table.insert(player.bullets, bullet)
     end
+  end
+
+  enemies_controller:spawnEnemy(2, 0)
+  enemies_controller:spawnEnemy(32, 0)
+end
+
+
+function enemies_controller:spawnEnemy(x, y)
+  enemy          = {}
+  enemy.x        = x
+  enemy.y        = y
+  enemy.bullets  = {}
+  enemy.cooldown = 40
+  enemy.speed    = 2
+  table.insert(self.enemies, enemy)
+end
+
+
+function enemy:fire()
+  if self.cooldown <= 0 then
+    self.cooldown = 40
+    bullet   = {}
+    bullet.x = self.x + 7
+    bullet.y = self.y
+    table.insert(self.bullets, bullet)
   end
 end
 
@@ -30,23 +63,33 @@ function love.update(dt)
     player.fire()
   end
 
-  for i,b in pairs(player.bullets) do
+  for _,e in pairs(enemies_controller.enemies) do
+    e.y = e.y + 0.1
+  end
+
+  for i,b in ipairs(player.bullets) do
     if b.y < -10 then
       table.remove(player.bullets, i)
     end
-    b.y = b.y - 10
+    b.y = b.y - 1
   end
 end
 
 
 function love.draw()
+  love.graphics.scale(5)
+  love.graphics.setColor(255, 255, 255, 255)
+
   -- Draw the player
-  love.graphics.setColor(255, 90, 0, 255)
-  love.graphics.rectangle("fill", player.x, player.y, 80, 20)
+  love.graphics.draw(player.image, player.x, player.y, 0)
+
+  for _,e in pairs(enemies_controller.enemies) do
+    love.graphics.draw(enemies_controller.image, e.x, e.y, 0)
+  end
 
   -- Draw the bullets
   love.graphics.setColor(255, 255, 255, 255)
   for _,b in pairs(player.bullets) do
-    love.graphics.rectangle("fill", b.x, b.y, 10, 10)
+    love.graphics.rectangle("fill", b.x, b.y, 2, 2)
   end
 end
