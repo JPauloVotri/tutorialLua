e638b41e6024986d39247307a8f115d00946d2b5
João Paulo Marques Votri <jpaulo.votri@hotmail.com>
2017-04-07 02:41:10 -0300

    Criada classe de player

---
+++ classes/player.lua
@@ -0,0 +1,18 @@
+player            = {}
+player.x          = 0
+player.y          = 110
+player.bullets    = {}
+player.cooldown   = 40
+player.speed      = 0.25
+player.image      = love.graphics.newImage("player.png")
+player.fire_sound = love.audio.newSource('Laser_Shoot2.wav')
+player.fire       = function()
+  if player.cooldown <= 0 then
+    player.cooldown = 80
+    love.audio.play(player.fire_sound)
+    bullet   = {}
+    bullet.x = player.x + 4
+    bullet.y = player.y + 2
+    table.insert(player.bullets, bullet)
+  end
+end
--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
-require "particles"
+require "classes/particles"
+require "classes/player"
 
 love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -29,25 +30,6 @@ function love.load()
   game_win  = false
   
   background_image = love.graphics.newImage('background.png')
-
-  player          = {}
-  player.x        = 0
-  player.y        = 110
-  player.bullets  = {}
-  player.cooldown = 40
-  player.speed    = 0.25
-  player.image    = love.graphics.newImage("player.png")
-  player.fire_sound = love.audio.newSource('Laser_Shoot2.wav')
-  player.fire     = function()
-    if player.cooldown <= 0 then
-      player.cooldown = 80
-      love.audio.play(player.fire_sound)
-      bullet   = {}
-      bullet.x = player.x + 4
-      bullet.y = player.y + 2
-      table.insert(player.bullets, bullet)
-    end
-  end
 
   for i=0, 10 do
     enemies_controller:spawnEnemy(i*15, 0)
